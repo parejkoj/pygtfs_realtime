9b5d996cc6f6ec976576002dd53b8014489b91f8
John K. Parejko <John.Parejko@alumni.carleton.edu>
2014-05-31 15:06:40 -0400

    more stuff

--- pygtfsrt/nmea_gtfsrt.py
+++ pygtfsrt/nmea_gtfsrt.py
@@ -6,10 +6,27 @@ Listen for nmea data and produce a GTFS-realtime output.
 from future import print_function, division, unicode_literals
 
 import pynmea2
+import transitfeed
 
-testfile = "../data/sample-nmea.txt"
+import urllib
 
-with open(testfile, 'r') as test_data:
-    nmea_stream = pynmea2.NMEAStreamReader(stream_obj=test_data)
-    data = nmea_stream.next()
-    lat,lon = data[0].longitude, data[0].latitude
+class GTFSrealtimeNMEA(object):
+    """Output a new GTFS-realtime from NMEA input."""
+
+    def __init__(nmea_stream, gtfs_stream):
+        with open(nmea_stream) as input_tream:
+            stream = pynmea2.NMEAStreamReader(input_stream)
+            self.stream = stream
+        with open(gtfs_file) as gtfs:
+
+
+    def get_next_position():
+        """Return the lat/long of the bus."""
+        data = self.stream.next()
+        return data[0].longitude, data[0].latitude
+
+    def __call__():
+        """Return a gtfs-realtime protocol buffer string."""
+        return None
+
+gtfsrt = GTFSrealtimeNMEA("../data/sample-nmea.txt"):
